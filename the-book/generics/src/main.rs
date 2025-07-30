21d3a4f80203d2c464ace09b4e5daed733e116b4
Fabrizio Damicelli <fabridamicelli@gmail.com>
2025-07-30 15:26:29 +0200

    book: generics

---
+++ the-book/generics/src/main.rs
@@ -0,0 +1,32 @@
+use std::cmp::PartialOrd;
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn main() {
+    let list = vec![1, 2, 3, 4];
+    let res = largest(&list);
+
+    let p = Point;
+    println!("largest: {res}")
+}
