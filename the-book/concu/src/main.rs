b064869475b2fea5f34fe61c262bf32d9a5e50ae
Fabrizio Damicelli <fabridamicelli@gmail.com>
2026-01-08 17:21:07 +0100

    wip: book: async

--- the-book/concu/src/main.rs
+++ the-book/concu/src/main.rs
@@ -1,5 +1,40 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
+}
+
+// fn main() {
+//     let (tx, rx) = mpsc::channel();
+//
+//     thread::spawn(move || {
+//         let val = String::from("hi");
+//         tx.send(val).unwrap();
+//     });
+//
+//     let out = rx.recv().unwrap();
+//     println!("{}", out)
+// }
 
 // fn main() {
 //     let handle = thread::spawn(|| {
@@ -17,12 +52,12 @@ use std::time::Duration;
 //     handle.join().unwrap();
 // }
 
-fn main() {
-    let v = vec![1, 2, 3];
-
-    let handle = thread::spawn(move || {
-        println!("Here's a vector: {v:?}");
-    });
-
-    handle.join().unwrap();
-}
+// fn main() {
+//     let v = vec![1, 2, 3];
+//
+//     let handle = thread::spawn(move || {
+//         println!("Here's a vector: {v:?}");
+//     });
+//
+//     handle.join().unwrap();
+// }
